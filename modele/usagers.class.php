a0bcdfc59986e05b7c3c21188ac38cdb241b7a75
username-Everam <simsimi.armand@gmail.com>
2023-11-17 10:20:17 +0100

    btrh

--- modele/usagers.class.php
+++ modele/usagers.class.php
@@ -1,22 +1,21 @@
 <?php
-class Usager{
+class Usager extends Personne{
 	private $_Id_Usager;
-	private $_Id_personne;
 	private $_N_sécurite_sociale;
 	private $_Adresse;
 	private $_Date_naissance;
 	private $_Lieu_naissance;
 	private $_Id_Medecin_referent;
 	
-	public function __construct($Id_Usager, $Id_personne, $Id_medecin, $Adresse, $Date_naissance $Lieu_naissance, $Id_Medecin_referent)
+	public function __construct($Id_Usager, string $Nom, string $Prenom, Civilite $Civilite, int $Id_personne, $Id_medecin, $Adresse, $Date_naissance, $Lieu_naissance, $Id_Medecin_referent)
 	{
-		$this->_Id_personne = $Id_personne;
+		super($Nom, $Prenom, $Civilite, $Id_personne);
 		$this->_Adresse = $Adresse;
 		$this->_Date_naissance = $Date_naissance;
 		$this->_Id_Usager = $Id_Usager;
 		$this->_Id_Medecin_referent = $Id_Medecin_referent;
 		$this->_Lieu_naissance = $Lieu_naissance;
-		$this->_Id_medecin = $Id_medecin;
+		$this->_N_sécurite_sociale = $N_sécurite_sociale;
 	}
 }
 ?>
