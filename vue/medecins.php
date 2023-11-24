32d8a7b90da001bc170c589056009db4ec5f64ac
Sofia Gribanova <grisoffa@gmail.com>
2023-11-24 13:05:06 +0100

    basic css start

--- vue/medecins.php
+++ vue/medecins.php
@@ -1,18 +1,29 @@
+<?php 
+clearstatcache();
+?>
 <head>
     <meta charset="utf-8" />
     <title>Medecins</title>
+    <link rel="stylesheet" href="css/medecin.css">
 </head>
 <body>
 <h1>Liste des medecins</h1>
+
+<div class="box_medecin">
+
 <?php
     require($_SERVER['DOCUMENT_ROOT'].'/miniProjet_php/modele/classes.php');
     $controleur = new Medecin_controleur();
     $resultat=$controleur->liste_medecins();
-    echo count($resultat);
     foreach ($resultat as $value){
-
-        echo "<div><div>".$value->getNom()."</div>"."<div>".$value->getPrenom()."</div></div>";
+        echo "<div class='item_medecin'>
+        <img
+        class='icone_liste_medecin'
+        src='img/icone_homme.png'
+        alt='icone d'un medecin'/>
+        <div class='nom'>".$value->getNom()."</div>"."<div>".$value->getPrenom()."</div></div>";
     }
 ?>
+</div>
 <input type="button" value="Ajouter un medecin">
 </body>
