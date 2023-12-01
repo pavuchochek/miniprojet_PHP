0fad597037b72315f285db58ce1929f8847b356b
username-Everam <simsimi.armand@gmail.com>
2023-12-01 10:26:11 +0100

    Création dossier SQL

---
+++ SQL/InsererMedecin.sql
@@ -0,0 +1,21 @@
+INSERT INTO usager (nom, prenom, civilité) VALUES
+('Doe', 'John', 'M'),
+('Smith', 'Jane', 'F'),
+('Johnson', 'Michael', 'M'),
+('Williams', 'Emily', 'F'),
+('Brown', 'David', 'M'),
+('Jones', 'Sarah', 'F'),
+('Miller', 'Robert', 'M'),
+('Davis', 'Jennifer', 'F'),
+('Wilson', 'Daniel', 'M'),
+('Taylor', 'Jessica', 'F'),
+('Anderson', 'Christopher', 'M');
+
+NSERT INTO medecin (id_usager) VALUES
+(4),
+(5),
+(6),
+(7),
+(8),
+(9),
+(10);
