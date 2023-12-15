2cde93af5a62c8b8b205e65e75188299726aa1f6
username-Everam <simsimi.armand@gmail.com>
2023-12-15 16:27:15 +0100

    bouton stats dans le footer (faut modifier le css du bouton, c'est pas tres bo)

--- src/vue/footer.php
+++ src/vue/footer.php
@@ -2,6 +2,9 @@
 <html lang="fr">
     <footer>
         <div id="footer">
+            <a href ="statistiques.php">
+                <input type="button" value="Statistiques">
+            </a>
             <p>Site réalisé par : </p>
             <p>Simon Armand et Sofia Gribanova</p>
         </div>
