22836afae1483eb5ebf1d0da89b3716df5512bab
username-Everam <simsimi.armand@gmail.com>
2023-12-15 15:58:36 +0100

    modification du footer

--- src/vue/footer.php
+++ src/vue/footer.php
@@ -1,7 +1,9 @@
 <!DOCTYPE html>
 <html lang="fr">
     <footer>
-        <p>Site réalisé par : </p>
-        <p>Simon Armand et Sofia Gribanova</p>
+        <div id="footer">
+            <p>Site réalisé par : </p>
+            <p>Simon Armand et Sofia Gribanova</p>
+        </div>
     </footer>
 </html>
