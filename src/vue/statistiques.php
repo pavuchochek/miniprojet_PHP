00b2a49c8610ab1e4f6b784e772ef28b62169f46
username-Everam <simsimi.armand@gmail.com>
2023-12-15 16:15:23 +0100

    Ajout de la page stats

---
+++ src/vue/statistiques.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8" />
+        <title>Rdv</title>
+        <link rel="stylesheet" href="css/CSSstats.css">
+        <link rel="stylesheet" href="css/CSSheader.css">
+        <link rel="stylesheet" href="css/CSSfooter.css">
+    </head>
+
+    <?php include 'header.php'; ?>
+
+    <body>
+        <div class="body">
+            <h1>Stats</h1>
+        </div>
+    </body>
+
+    <?php include 'footer.php'; ?>
+</html>
