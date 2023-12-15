643ba90432421b910efa0d84fcc3863408e1caa0
username-Everam <simsimi.armand@gmail.com>
2023-12-15 16:51:58 +0100

    Modif du css pour mettre les boutons de chaque pas dans le footer

--- src/vue/header.php
+++ src/vue/header.php
@@ -5,5 +5,11 @@
             <img src="img/logo.png" alt="logo">
         </a>
         <h1>Cabinet Médical</h1>
+        <div id = "boutons">
+            <a href ="index.php" id="header_accueil"><input type="button" value="Accueil"></a>
+            <a href ="medecins.php" id="header_medecin"><input type="button" value="Médecins"></a>
+            <a href ="usagers.php" id="header_user"><input type="button" value="Usagers"></a>
+            <a href ="rdv.php" id="header_rdv"><input type="button" value="Rendez-vous"></a>
+        </div>
     </header>
 </html>
