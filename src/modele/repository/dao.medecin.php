84782f4565f2877536647c94efc95358ae8af224
Sofia Gribanova <grisoffa@gmail.com>
2023-12-20 18:00:19 +0100

    essais pour faire fonctionner la db

--- src/modele/repository/dao.medecin.php
+++ src/modele/repository/dao.medecin.php
@@ -18,9 +18,8 @@ class Dao_Medecin{
 
     public function  liste_medecins(String $nom,String $prenom){
             if ($nom=="" && $prenom=="") {
-                $res=$this->pdo->query('SELECT personne.Nom,personne.Prénom,personne.Civilité,médecin.id_Médecin,personne.id_Personne 
-                FROM médecin,personne 
-                WHERE médecin.Id_Personne=personne.Id_Personne');
+                $res=$this->pdo->query('SELECT table_name FROM information_schema.tables');
+                var_dump($res);
                 $tablo_medecins=array();
                 while ($data = $res->fetch()) {
                     $medecin=new Medecin($data[0],$data[1],$data[2],$data[3],$data[4]);
