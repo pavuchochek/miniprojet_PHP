6f1348838b0fb2d04886c4fa6ec836abb0b0e114
username-Everam <simsimi.armand@gmail.com>
2023-12-22 10:16:55 +0100

    GNGN

--- src/modele/classes/usagers.class.php
+++ src/modele/classes/usagers.class.php
@@ -16,5 +16,60 @@ class Usager extends Personne{
 		$this->_Medecin_referent = $medecin;
 		$this->_Lieu_naissance = $Lieu_naissance;
 	}
+
+	public function getAdresse():String{
+		return $this->_Adresse;
+	}
+
+	public function getDateNaissance():String{
+		return $this->_Date_naissance;
+	}
+
+	public function getMedecinReferent():Medecin{
+		return $this->_Medecin_referent;
+	}
+
+	public function getLieuNaissance():String{
+		return $this->_Lieu_naissance;
+	}
+
+	public function getNsecuriteSociale():String{
+		return $this->_N_sécurite_sociale;
+	}
+
+	public function setAdresse(String $Adresse){
+		$this->_Adresse = $Adresse;
+	}
+
+	public function setDateNaissance(String $Date_naissance){
+		$this->_Date_naissance = $Date_naissance;
+	}
+
+	public function setMedecinReferent(Medecin $Medecin_referent){
+		$this->_Medecin_referent = $Medecin_referent;
+	}
+
+	public function setLieuNaissance(String $Lieu_naissance){
+		$this->_Lieu_naissance = $Lieu_naissance;
+	}
+
+	public function setNsecuriteSociale(String $N_sécurite_sociale){
+		$this->_N_sécurite_sociale = $N_sécurite_sociale;
+	}
+
+	public function __toString():String{
+		return $this->_Nom." ".$this->_Prenom;
+	}
+
+	public function __destruct(){
+		unset($this->_Nom);
+		unset($this->_Prenom);
+		unset($this->_Civilite);
+		unset($this->_Adresse);
+		unset($this->_Date_naissance);
+		unset($this->_Lieu_naissance);
+		unset($this->_Medecin_referent);
+		unset($this->_N_sécurite_sociale);
+	}
 }
 ?>
