d9b2d756934cd82ce0b8b80457efbb82009c5136
Sofia Gribanova <grisoffa@gmail.com>
2023-12-22 10:49:57 +0100

    changement du dao pour recuperer les id

--- src/modele/repository/dao.medecin.php
+++ src/modele/repository/dao.medecin.php
@@ -19,13 +19,15 @@ class Dao_Medecin{
 
     public function liste_medecins(String $nom,String $prenom){
             if ($nom=="" && $prenom=="") {
-                $res=$this->pdo->query('SELECT Personne.Nom,Personne.Prenom,Personne.Civilite 
+                $res=$this->pdo->query('SELECT Personne.Nom,Personne.Prenom,Personne.Civilite,Personne.Id_Personne,Medecin.Id_Medecin
                 FROM Medecin,Personne 
                 WHERE Medecin.Id_Personne=Personne.Id_Personne');
                 $tablo_medecins=array();
                 while ($data = $res->fetch()) {
                     $personne=new Personne($data[0],$data[1],$data[2]);
+                    $personne->setId($data[3]);
                     $medecin=new Medecin($personne);
+                    $medecin->setIdMedecin($data[4]);
                     $tablo_medecins[] = $medecin;
                 }
             }
@@ -45,7 +47,6 @@ class Dao_Medecin{
             'id'=>$id
         ));
     }
-
     public function supprimer_medecins(Medecin $medecin){
         $req=$this->pdo->prepare('DELETE FROM Medecin WHERE Id_Personne=:id');
         $req->execute(array(
@@ -56,7 +57,6 @@ class Dao_Medecin{
             'id'=>$medecin->getId()
         ));
     }
-
     public function modifier_medecins(Medecin $medecin){
         $req=$this->pdo->prepare('UPDATE Personne SET Nom=:nom,Prenom=:prenom,Civilite=:civilite WHERE Id_Personne=:id');
         $req->execute(array(
