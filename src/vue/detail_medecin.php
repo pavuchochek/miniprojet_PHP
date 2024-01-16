71ad2555482a1b47d88e88bcecbd93834467c13b
username-Everam <simsimi.armand@gmail.com>
2024-01-16 10:00:52 +0100

    hourra

--- src/vue/detail_medecin.php
+++ src/vue/detail_medecin.php
@@ -83,18 +83,16 @@
                         <select name="idUsager" id="combo_box">
                             <?php
                             $resultat = $controleur->getListeUsagersMedecin($idmedecin);
-                            $usagers = $controleur->getListeUsagers($idmedecin);
+                            $usagers = $controleur->listeUsagersNonReferents($idmedecin);
                             if ($usagers == null) {
                                 echo "<option value=''>Aucun patient</option>";
                             } else {
                                 foreach ($usagers as $value) {
-                                    if (!in_array($value, $resultat)) { //enlever l'usager si il n'est pas déjà assigné au médecin (je sais pas pourquoi ça marche pas)
-                                        $prenom = $value->getPrenom();
-                                        $nom = $value->getNom();
-                                        $id = $value->getIdUsager();
-                                        $num = $value->getNsecuriteSociale();
-                                        echo "<option value='$id'>$prenom $nom ($num)</option>";
-                                    }
+                                    $prenom = $value->getPrenom();
+                                    $nom = $value->getNom();
+                                    $id = $value->getIdUsager();
+                                    $num = $value->getNsecuriteSociale();
+                                    echo "<option value='$id'>$prenom $nom ($num)</option>";
                                 }
                             }
                             ?>
--- src/vue/traitements/traitement_assigner_usager.php
+++ src/vue/traitements/traitement_assigner_usager.php
@@ -3,17 +3,19 @@
 
     $controleur = new Usager_controleur();
     $medecinReferent = isset($_POST["medecinReferent"]) ? intval($_POST["medecinReferent"]) : null;
-    $idUsager = $_POST["idUsager"];
-    $usager = $controleur->getUsagerById($idUsager);
-    $adresse = $usager->getAdresse();
-    $dateNaissance = $usager->getDateNaissance();
-    $lieuNaissance = $usager->getLieuNaissance();
-    $numeroSecu = $usager->getNsecuriteSociale();
-    $nom = $usager->getNom();
-    $prenom = $usager->getPrenom();
-    $civilite = $usager->getCivilite();
+    if ($_POST["idUsager"] !== '') {
+        $idUsager = $_POST["idUsager"];
+        $usager = $controleur->getUsagerById($idUsager);
+        $adresse = $usager->getAdresse();
+        $dateNaissance = $usager->getDateNaissance();
+        $lieuNaissance = $usager->getLieuNaissance();
+        $numeroSecu = $usager->getNsecuriteSociale();
+        $nom = $usager->getNom();
+        $prenom = $usager->getPrenom();
+        $civilite = $usager->getCivilite();
 
-    $controleur->modifier_usager($idUsager, $nom, $prenom, $civilite, $adresse, $dateNaissance, $lieuNaissance, $numeroSecu, $medecinReferent);
+        $controleur->modifier_usager($idUsager, $nom, $prenom, $civilite, $adresse, $dateNaissance, $lieuNaissance, $numeroSecu, $medecinReferent);
+    }
 
     header('Location: /detail_medecin.php?id='.$medecinReferent);
 ?>
