c4b4122ca841a030f048b6385c9dc40cbe114097
username-Everam <simsimi.armand@gmail.com>
2024-01-17 11:36:30 +0100

    j'ai fait des entrées

--- src/controleur/rdv.controleur.php
+++ src/controleur/rdv.controleur.php
@@ -14,30 +14,36 @@ class Rdv_controleur{
         public function liste_rdv(){
             return $this->daoRdv->liste_rdv_Actuels();
         }
+
         public function creationRdv($idMedecin,$idUsager,$date,$heureDebut,$heurefin){
             $usager=$this->daoRdv->getUsagerById($idUsager);
             $medecin=$this->daoMedecin->getMedecinById($idMedecin);
             $rdv=new Rdv($date,$heureDebut,$heurefin,$medecin,$usager);
             $this->daoRdv->createRdv($rdv);
         }
+
         public function modifierRdv($idMedecinAncien,$idUsagerAncien,$dateAncien,$heureDebutAncien,$heurefinAncien,$idMedecin,$idUsager,$date,$heureDebut,$heurefin){
             $this->suppressionRdv($idMedecinAncien, $idUsagerAncien, $dateAncien, $heureDebutAncien,$heurefinAncien) ;
             $this->creationRdv($idMedecin, $idUsager, $date, $heureDebut, $heurefin) ;
         }
+
         public function suppressionRdv(int $idMedecin,int $idUsager,$date,$heureDebut,$heurefin){
             $usager=$this->daoRdv->getUsagerById($idUsager);
             $medecin=$this->daoMedecin->getMedecinById($idMedecin);
             $rdv=new Rdv($date,$heureDebut,$heurefin,$medecin,$usager);
             $this->daoRdv->deleteRdv($rdv);
         }
+
         public function getRdvByIdUsager(int $idUsager){
             $array=$this->daoRdv->liste_rdv_Actuels_usagerbyId($idUsager);
             return $array;
         }
+
         public function getRdvByIdMedecin(int $idMedecin){
             $array=$this->daoRdv->liste_rdv_Actuels_medecinbyId($idMedecin);
             return $array;
         }
+        
         public function getRdvByIdMedecinIdUsager(int $idUsager,int $idMedecin){
             $array=$this->daoRdv->liste_rdv_Actuels_medecin_usager_byId($idUsager,$idMedecin);
             return $array;
