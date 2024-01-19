287b8acc36b471bc20acb0733cf449a7e347c4cd
Sofia Gribanova <grisoffa@gmail.com>
2024-01-19 10:43:02 +0100

    erreur pour rdv

--- src/vue/modifier_rdv.php
+++ src/vue/modifier_rdv.php
@@ -24,6 +24,12 @@
 
     <body>
         <div class="body">
+        <?php
+            if (isset($_SESSION['erreur_message'])) {
+                echo "<p class='erreur'>" . $_SESSION['erreur_message'] . "</p>";
+                unset($_SESSION['erreur_message']); // Effacer le message après l'affichage
+            }
+        ?>
             <div id="formulaire" class="formulaire" style="display:block;">
                 <form id="rdvForm" method="post" action="traitements/traitement_modifier_rdv.php" onsubmit="return Valide()">
                     <?php
